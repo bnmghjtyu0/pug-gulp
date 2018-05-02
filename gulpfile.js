983ca049d3978d8f495262dba66b8ade5756eb5e
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2018-05-02 16:28:00 +0800

    pug + gulp 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const browserSync = require('browser-sync').create() // 瀏覽器同步
 const pug = require('gulp-pug')
 const sass = require('gulp-sass') // sass
 const plumber = require('gulp-plumber') // 載入 gulp-plumber
+const gulpImagemin = require('gulp-imagemin') //壓縮圖片
 
 // 路徑設定
 const path = {
@@ -13,11 +14,19 @@ const path = {
 }
 
 // 服務
-gulp.task('browser-sync', ['pug', 'sass'], function() {
-  browserSync.init({
-    server: './build'
+gulp.task('browser-sync', ['pug', 'sass', 'image'], function() {
+  browserSync.init(['css/*.css', 'js/*.js'], {
+    server: {
+      baseDir: 'build',
+      directory: false,
+      index: 'pages/index.html',
+      routes: {
+        '/index.html': 'build/pages/index.html',
+        '/about.html': 'build/pages/about.html'
+      }
+    }
   })
-  gulp.watch(path.source + '*.pug', ['pug']).on('change', browserSync.reload)
+  gulp.watch(path.source + '**/*.pug', ['pug']).on('change', browserSync.reload)
   gulp.watch(path.build + '*.html').on('change', browserSync.reload)
   gulp.watch(path.source + '*assets/css/*.scss', ['sass']).on('change', browserSync.reload)
   gulp.watch(path.build + '*.css').on('change', browserSync.reload)
@@ -26,12 +35,12 @@ gulp.task('browser-sync', ['pug', 'sass'], function() {
 // 編譯pug
 gulp.task('pug', function() {
   return gulp
-    .src(path.source + '*.pug')
+    .src([!path.source + 'Layout.pug', path.source + '**/pages/*.pug'])
     .pipe(plumber())
-    .pipe(pug())
+    .pipe(pug({ pretty: true }))
     .pipe(gulp.dest(path.build))
 
-    .pipe(browserSync.stream({ match: path.source + 'assets/*.pug' }))
+    .pipe(browserSync.stream({ match: path.source + '**/*.pug' }))
 })
 // 編譯 SASS
 gulp.task('sass', function() {
@@ -40,6 +49,14 @@ gulp.task('sass', function() {
     .pipe(sass())
     .pipe(gulp.dest(path.build + 'css/'))
     .pipe(browserSync.stream())
+})
+
+// 壓縮圖片
+gulp.task('image', function() {
+  gulp
+    .src(path.source + 'assets/img/**')
+    .pipe(gulpImagemin())
+    .pipe(gulp.dest(path.build + 'img'))
 })
 
 gulp.task('default', ['browser-sync'])
